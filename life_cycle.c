20e6c1aa1c8819135e113defde54b19fa301b03e
Aimad Bahdir <abahdir@e0r5p2.1337.ma>
2021-07-10 13:15:10 +0100

    +

--- life_cycle.c
+++ life_cycle.c
@@ -6,7 +6,7 @@
 /*   By: abahdir <abahdir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 16:14:27 by abahdir           #+#    #+#             */
-/*   Updated: 2021/07/10 13:10:23 by abahdir          ###   ########.fr       */
+/*   Updated: 2021/07/10 13:13:30 by abahdir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@ void	*calc_tt_die(void *arg)
 	philo = (t_philo *)arg;
 	tt_die = get_time() - philo->last_eat;
 
-	pthread_mutex_lock(&philo->more->print);
 	// ft_putnbr(tt_die, NULL);
 	// ft_putstr("\n", NULL);
-	pthread_mutex_unlock(&philo->more->print);
 	// if (tt_die > philo->more->tt_die)
 	// 	died(philo);
 	return (NULL);
@@ -40,19 +38,19 @@ void	*philo_life(void *arg)
 	philo = (t_philo *)arg;
 	lf = philo->id - 1;
 	rf = philo->id % philo->more->nb_philos;
-	if (pthread_create(&calc, NULL, &calc_tt_die, &philo) != 0)
-		exit(1);
+	// if (pthread_create(&calc, NULL, &calc_tt_die, &philo) != 0)
+	// 	exit(1);
 	while (1)
 	{
 		pthread_mutex_lock(&philo->more->forks[lf]);
-		fork_taken(1, philo);
+		ft_do(1, philo);
 		pthread_mutex_lock(&philo->more->forks[rf]);
-		fork_taken(1, philo);
-		eating(2, philo);
+		ft_do(1, philo);
+		ft_do(2, philo);
 		pthread_mutex_unlock(&philo->more->forks[lf]);
 		pthread_mutex_unlock(&philo->more->forks[rf]);
-		sleeping(4, philo);
-		thinking(3, philo);
+		ft_do(4, philo);
+		ft_do(3, philo);
 	}
 	
 	return (philo);
--- life_events.c
+++ life_events.c
@@ -6,7 +6,7 @@
 /*   By: abahdir <abahdir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/07 18:31:20 by abahdir           #+#    #+#             */
-/*   Updated: 2021/07/10 13:11:04 by abahdir          ###   ########.fr       */
+/*   Updated: 2021/07/10 13:14:19 by abahdir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,25 @@ long	get_time(void)
 
 void	ft_do(short stat, t_philo *philo)
 {
+	pthread_mutex_lock(&philo->more->print);
 	ft_putstr("\x1B[0m");
 	ft_putnbr(get_time());
 	ft_putstr(" ");
 	ft_putnbr(philo->id);
-	pthread_mutex_lock(&philo->more->print);
 	if (stat == 1)
-		ft_putstr("\x1B[31mis has taken a fork\n\x1B[0m");
+		ft_putstr("\x1B[31mis has taken a fork\x1B[0m\n");
 	else if (stat == 2)
 	{
 		philo->last_eat = get_time();
-		ft_putstr("\x1B[32mis is eating\n\x1B[0m");
+		ft_putstr("\x1B[32mis is eating\x1B[0m\n");
 		philo->nb_eat++;
 		ft_pause(philo->more->tt_eat);
 	}
 	else if (stat == 3)
-		ft_putstr("\x1B[33mis is thinking\n\x1B[0m");
+		ft_putstr("\x1B[33mis is thinking\x1B[0m\n");
 	else if (stat == 4)
 	{
-		ft_putstr("\x1B[34mis is sleeping\n\x1B[0m");
+		ft_putstr("\x1B[34mis is sleeping\x1B[0m\n");
 		ft_pause(philo->more->tt_sleep);
 	}
 	pthread_mutex_unlock(&philo->more->print);
